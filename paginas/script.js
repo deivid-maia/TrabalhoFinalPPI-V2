a15578224c8c360a7e371c10ced99076731e3c1b
deivid-maia <deivid.mm@gmail.com>
2023-12-10 23:38:59 -0300

    feats: sessio, cookies, html chat / fix: correçao de sinc repositorio remoto

---
+++ paginas/script.js
@@ -0,0 +1,22 @@
+function sendMessage() {
+  const userSelect = document.getElementById('userSelect');
+  const messageInput = document.getElementById('messageInput');
+  const messages = document.getElementById('messages');
+
+  const selectedUser = userSelect.options[userSelect.selectedIndex].text;
+  const message = messageInput.value;
+
+  if (message.trim() !== '') {
+      const data = new Date();
+      const dataFormatada = `${data.toLocaleDateString()} ${data.toLocaleTimeString()}`;
+
+      const newMessage = document.createElement('div');
+      newMessage.classList.add('message');
+      newMessage.innerHTML = `<strong>${selectedUser}:</strong> ${message}
+                              <div class="dataHora">postado em:${dataFormatada}</div>`
+
+      messages.appendChild(newMessage);
+      messages.scrollTop = messages.scrollHeight;
+      messageInput.value = '';
+  }
+}
